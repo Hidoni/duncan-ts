7783b64ff7ec05847b66017a8168a751276880b3
Hidoni <ron30086@walla.com>
2022-01-26 00:51:08 +0200

    Added utils to change a question in QOTD

--- src/utils/QuestionOfTheDayUtils.ts
+++ src/utils/QuestionOfTheDayUtils.ts
@@ -1,5 +1,10 @@
 import Conf from 'conf';
+import { Message, TextChannel } from 'discord.js';
+import Bot from '../client/Bot';
 
+const FLURRY_OF_QUESTIONS_MESSAGE =
+    'Flurry of Questions Mode!\n\nAsk any (serious) question and the person below has to answer it. Then they add their own question and the cycle continues.';
+const JAKEMI_USER_ID = '381002402947399691';
 const config = new Conf();
 
 export function getDays(): number {
@@ -22,4 +27,72 @@ export function setChannel(channel: string) {
 
 export function capitalizeQuestion(question: string) {
     return question.charAt(0).toUpperCase() + question.slice(1);
+}
+
+function isFriday() {
+    const today = new Date();
+    return today.getDay() === 5;
+}
+
+function daysSinceDate(date: Date) {
+    const today = new Date();
+    const diff = today.getTime() - date.getTime();
+    return Math.floor(diff / (1000 * 60 * 60 * 24));
+}
+
+export async function changeQuestion(client: Bot) {
+    const channel = await client.channels.fetch(getChannel());
+    if (!channel) {
+        throw new Error(`Channel ${getChannel()} not found`);
+    }
+    if (!(channel instanceof TextChannel)) {
+        throw new Error(`Channel ${getChannel()} is not a text channel`);
+    }
+    let message = `**QotD #${getDays()}: {}**`;
+    if (isFriday()) {
+        message.replace('{}', FLURRY_OF_QUESTIONS_MESSAGE);
+    } else {
+        const question = await client.database.getRandomQuestion();
+        if (question) {
+            message.replace('{}', question.question);
+            client.logger?.info(
+                `Using question "${question.question}" by ${
+                    question.authorName
+                } after ${daysSinceDate(question.addedAt)} days.`
+            );
+            question.used = true;
+            question.save();
+        } else {
+            message.replace('{}', FLURRY_OF_QUESTIONS_MESSAGE);
+        }
+    }
+    await channel.send(message).then(async (msg: Message) => {
+        await (channel as TextChannel).messages
+            .fetchPinned()
+            .then(async (messages) => {
+                await messages.last()?.unpin();
+                msg.pin();
+            })
+            .catch(() =>
+                client.logger?.debug(
+                    'Failed to pin/unpin message, assuming intentional.'
+                )
+            );
+    });
+    client.database.getUnusedQuestions().then((questions) => {
+        if (questions.length <= 5) {
+            client.users
+                .fetch(JAKEMI_USER_ID)
+                .then((user) => {
+                    user.send(
+                        `Just as a heads up, we're running low on QotD questions!! I only have ${questions.length} left!!`
+                    );
+                })
+                .catch((error) =>
+                    client.logger?.debug(
+                        `Failed to send heads up message, error: ${error}`
+                    )
+                );
+        }
+    });
 }
