93ba965307e63506a47e8ff5c85658c31055d09f
Hidoni <ron30086@walla.com>
2022-01-26 15:51:56 +0200

    Added an OnMessageEvent for gimmick points

---
+++ src/events/gimmicks/OnMessageEvent.ts
@@ -0,0 +1,46 @@
+import {
+    CategoryChannel,
+    GuildTextBasedChannel,
+    Message,
+    TextChannel,
+    ThreadChannel,
+} from 'discord.js';
+import Bot from '../../client/Bot';
+import { EventHandler } from '../../interfaces/Event';
+
+const GIMMICKS_CATEGORY_ID = '789272443104788530';
+
+function getCategory(channel: GuildTextBasedChannel): CategoryChannel | null {
+    const category = channel.parent;
+    if (!category) {
+        return null;
+    }
+    if (category instanceof CategoryChannel) {
+        return category;
+    }
+    return getCategory(category);
+}
+
+async function giveGimmickPoints(client: Bot, message: Message) {
+    if (
+        message.channel instanceof TextChannel ||
+        message.channel instanceof ThreadChannel
+    ) {
+        const category = getCategory(message.channel);
+        if (category) {
+            if (category.id === GIMMICKS_CATEGORY_ID) {
+                await client.database
+                    .getGimmickPoints(message.author.id)
+                    .then((points) => {
+                        points.points++;
+                        points.save();
+                    });
+            }
+        }
+    }
+}
+
+export const name: string = 'message';
+export const handler: EventHandler = async (client: Bot, message: Message) => {
+    await giveGimmickPoints(client, message);
+};
