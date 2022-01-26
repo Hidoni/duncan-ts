af335dbcdb873fb4097ab6ec23a86bc5810b0f86
Hidoni <ron30086@walla.com>
2022-01-26 02:49:07 +0200

    Auto-formatted QotD ReadyEvent

--- src/events/question_of_the_day/ReadyEvent.ts
+++ src/events/question_of_the_day/ReadyEvent.ts
@@ -9,15 +9,24 @@ import {
 
 export const name: string = 'ready';
 export const handler: EventHandler = async (client: Bot) => {
-    let rule = new schedule.RecurrenceRule(undefined, undefined, undefined, undefined, 12, 0, 0);
-    rule.tz = 'America/New_York';
-    client.logger?.debug(`Setting up the Question Of The Day job, first invocation at ${rule.nextInvocationDate(new Date())}`);
-    schedule.scheduleJob(
-        rule,
-        async () => {
-            setDays(getDays() + 1);
-            changeQuestion(client);
-        }
+    let rule = new schedule.RecurrenceRule(
+        undefined,
+        undefined,
+        undefined,
+        undefined,
+        12,
+        0,
+        0
     );
+    rule.tz = 'America/New_York';
+    client.logger?.debug(
+        `Setting up the Question Of The Day job, first invocation at ${rule.nextInvocationDate(
+            new Date()
+        )}`
+    );
+    schedule.scheduleJob(rule, async () => {
+        setDays(getDays() + 1);
+        changeQuestion(client);
+    });
 };
 export const once: boolean = true;
