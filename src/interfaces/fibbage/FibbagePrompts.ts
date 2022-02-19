103c280a38844a8208f619433f848f8c21a064b8
Hidoni <ron30086@walla.com>
2022-02-20 00:46:17 +0200

    Added Fibbage Prompt Object

--- src/interfaces/fibbage/FibbagePrompts.ts
+++ src/interfaces/fibbage/FibbagePrompts.ts
@@ -1,15 +1,8 @@
-export type FibbagePrompts = {
-    [prompt: string]: {
-        question: string;
-        answers: readonly [
-            string,
-            string,
-            string,
-            string,
-            string,
-            string,
-            string
-        ];
-    };
+export type FibbagePrompt = {
+    question: string;
+    answers: readonly [string, string, string, string, string, string, string];
+};
 
+export type FibbagePrompts = {
+    [prompt: string]: FibbagePrompt;
 };
