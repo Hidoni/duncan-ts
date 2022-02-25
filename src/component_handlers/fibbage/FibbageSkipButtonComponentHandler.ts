6260df2b76e2d9556729c2c26f61f182cceba0d3
Hidoni <ron30086@walla.com>
2022-02-25 20:15:01 +0200

    Added skip button handler

---
+++ src/component_handlers/fibbage/FibbageSkipButtonComponentHandler.ts
@@ -0,0 +1,38 @@
+import { FibbageQuestionState } from '../../database/models/FibbageQuestion';
+import { ComponentHandlerFunction } from '../../interfaces/ComponentHandler';
+import {
+    generateQuestionModal,
+    getEnabled,
+    promptUserWithQuestion,
+} from '../../utils/FibbageUtils';
+
+export const handler: ComponentHandlerFunction = async (
+    client,
+    interaction
+) => {
+    const idInfo = interaction.customId.match(pattern);
+    const questionId = idInfo![1];
+    const question = await client.database.getFibbageQuestion(
+        Number.parseInt(questionId)
+    );
+    if (!question) {
+        client.logger?.error(
+            `Could not find question ${questionId} for user ${interaction.user.id}`
+        );
+        await interaction.reply({
+            content: `OnO, I'm sowwy, but I couldn't find your question, please let Hidoni know ASAP!!`,
+        });
+    } else if (question.state != FibbageQuestionState.ASKED) {
+        await interaction.reply(
+            "Nice try, but you've already answered this question!"
+        );
+    } else {
+        question.state = FibbageQuestionState.SKIPPED;
+        const askedQuestions = await client.database.getAllFibbageQuestions();
+        await promptUserWithQuestion(client, interaction.user, askedQuestions);
+    }
+};
+
+export const pattern: RegExp = /^fibbage_question_button_(\d+)$/;
+
+export const shoudLoad = () => getEnabled;
