656ba4a3aa3b741fa81d9b6d07b950167d9634e0
Hidoni <git@hidoni.com>
2025-03-05 23:57:03 +0200

    Add support for modifying brownie points using interact commands

--- src/commands/interacts/InteractCommand.ts
+++ src/commands/interacts/InteractCommand.ts
@@ -4,6 +4,9 @@ import Bot from '../../client/Bot';
 import { ChatInputCommandInteraction, CommandInteraction } from 'discord.js';
 import { getSafeReplyFunction } from '../../utils/InteractionUtils';
 
+const BROWNIE_POINT_REGEX =
+    /{modifyBrowniePoints\(delta=(?<delta>[+-]?\d+)\)}/g;
+
 export class InteractCommand {
     public handler: CommandHandler;
     public builder: SlashCommandBuilder;
@@ -22,7 +25,10 @@ export class InteractCommand {
         this.positiveResponses = positiveResponses;
         this.negativeResponses = negativeResponses;
         this.chanceForNegativeResponse = chanceForNegativeResponse;
-        this.handler = async (client: Bot, interaction: ChatInputCommandInteraction) => {
+        this.handler = async (
+            client: Bot,
+            interaction: ChatInputCommandInteraction
+        ) => {
             await getSafeReplyFunction(
                 client,
                 interaction
@@ -36,7 +42,39 @@ export class InteractCommand {
             .setDescription(description);
     }
 
-    protected async getResponse(client: Bot, interaction: ChatInputCommandInteraction) {
+    protected async resolveBrowniePoints(
+        client: Bot,
+        interaction: ChatInputCommandInteraction,
+        response: string
+    ): Promise<string> {
+        for (const match of response.matchAll(BROWNIE_POINT_REGEX)) {
+            if (!match.groups) {
+                continue;
+            }
+            const delta = Number.parseInt(match.groups['delta']);
+            await client.database
+                .getBrowniePoints(interaction.user.id)
+                .then((points) => {
+                    points.points += delta;
+                    points.save();
+                });
+            response = response.replace(match[0], '');
+        }
+        return response;
+    }
+
+    protected async resolveSpecial(
+        client: Bot,
+        interaction: ChatInputCommandInteraction,
+        response: string
+    ): Promise<string> {
+        return this.resolveBrowniePoints(client, interaction, response);
+    }
+
+    protected getMessage(
+        _client: Bot,
+        _interaction: ChatInputCommandInteraction
+    ) {
         if (Math.random() <= this.chanceForNegativeResponse) {
             return this.negativeResponses[
                 Math.floor(Math.random() * this.negativeResponses.length)
@@ -45,6 +83,17 @@ export class InteractCommand {
         return this.positiveResponses[
             Math.floor(Math.random() * this.positiveResponses.length)
         ];
+    }
+
+    protected async getResponse(
+        client: Bot,
+        interaction: ChatInputCommandInteraction
+    ) {
+        return this.resolveSpecial(
+            client,
+            interaction,
+            this.getMessage(client, interaction)
+        ).then((response) => response.trim());
     }
 }
 
