6e2fe184a5586a8233b6e9543dc7ac03b8442d23
Hidoni <git@hidoni.com>
2026-03-13 20:14:47 +0200

    Update getAllMessagesInChannel to support after argument

--- src/utils/MessageUtils.ts
+++ src/utils/MessageUtils.ts
@@ -1,14 +1,26 @@
 import { TextBasedChannel } from 'discord.js';
 
-export async function* getAllMessagesInChannel(channel: TextBasedChannel, before?: string) {
-    let messages = await channel.messages.fetch({ limit: 100, before: before });
+export async function* getAllMessagesInChannel(
+    channel: TextBasedChannel,
+    before?: string,
+    after?: string
+) {
+    if (before != null && after != null) {
+        throw new Error('before and after are mutually exclusive arguments!');
+    }
+    let messages = await channel.messages.fetch({
+        limit: 100,
+        before: before,
+        after: after,
+    });
     while (messages.size !== 0) {
         for (const [id, message] of messages) {
             yield message;
         }
         messages = await channel.messages.fetch({
             limit: 100,
-            before: messages.lastKey(),
+            before: before != null ? messages.lastKey() : undefined,
+            after: after != null ? messages.firstKey() : undefined,
         });
     }
 }
